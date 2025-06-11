284d69c98238a214918866692aa89a5461c1ec62
alphanoobie <hussaingadi.hg@gmail.com>
2025-06-11 13:39:59 +0930

    migrate data code

--- code/evolve.py
+++ code/evolve.py
@@ -1,7 +1,7 @@
 from population import Population
 from evaluate import Evaluate
 import numpy
-import tensorflow.examples.tutorials.mnist as input_data
+# from tensorflow.keras.datasets import mnist
 import tensorflow as tf
 import collections
 from utils import *
